05441f1615898aa48b17a0350cdf6a5302c48297
Rasmus Kragh Wendelbo <29890659+raskrawen@users.noreply.github.com>
2019-06-08 22:02:37 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.showNumber(input.lightLevel())
-led.plot(0, 0)
-led.plot(4, 4)
+let gameScore = 0
+let time = 350
+// create player sprite:
+let sprite: game.LedSprite = game.createSprite(2, 4)
+// create enemy:
+let enemy: game.LedSprite = game.createSprite(Math.randomRange(0, 4), 0)
+
+basic.forever(function () {
+    if (enemy.get(LedSpriteProperty.Y) == 4) {
+        enemy.delete()  // delete enemy
+        enemy = game.createSprite(Math.randomRange(0, 4), 0) // create new enemy
+    }
+    basic.pause(time)
+    enemy.change(LedSpriteProperty.Y, 1) // move enemy down
+    if (sprite.isTouching(enemy)) {
+        basic.showIcon(IconNames.Heart)
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    sprite.move(-1)
+})
